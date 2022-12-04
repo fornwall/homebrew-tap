a4eb828c7ae487c860cf130442117c154b126a07
Fredrik Fornwall <fredrik@fornwall.net>
2022-12-04 23:54:35 +0100

    Update

--- Formula/advent-of-code.rb
+++ Formula/advent-of-code.rb
@@ -2,6 +2,7 @@ class AdventOfCode < Formula
     desc "CLI to solve Advent of Code problems"
     homepage "https://aoc.fornwall.net"
 
+    version "0.1"
     url "https://fornwall.me/tmp/advent-of-code.tar.xz"
     sha256 "4c083722a0dcb54dcb8827198311075111046d170ab48d595a4b36a1501dce4a"
 
