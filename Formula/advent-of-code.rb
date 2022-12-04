0050ef0e1a47e5094b76a86956f9beeb19b524ab
Fredrik Fornwall <fredrik@fornwall.net>
2022-12-04 23:53:08 +0100

    Initial formula

---
+++ Formula/advent-of-code.rb
@@ -0,0 +1,11 @@
+class AdventOfCode < Formula
+    desc "CLI to solve Advent of Code problems"
+    homepage "https://aoc.fornwall.net"
+
+    url "https://fornwall.me/tmp/advent-of-code.tar.xz"
+    sha256 "4c083722a0dcb54dcb8827198311075111046d170ab48d595a4b36a1501dce4a"
+
+    def install
+        bin.install "advent-of-code"
+    end
+end
