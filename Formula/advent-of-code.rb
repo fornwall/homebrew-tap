da090c8a1d718ccf1b39496c1d883d2b2913b40e
Fredrik Fornwall <fredrik@fornwall.net>
2022-12-04 23:59:06 +0100

    Try linux

--- Formula/advent-of-code.rb
+++ Formula/advent-of-code.rb
@@ -3,8 +3,16 @@ class AdventOfCode < Formula
     homepage "https://aoc.fornwall.net"
 
     version "0.1"
-    url "https://fornwall.me/tmp/advent-of-code.tar.xz"
-    sha256 "4c083722a0dcb54dcb8827198311075111046d170ab48d595a4b36a1501dce4a"
+
+    on_macos do
+        url "https://fornwall.me/tmp/advent-of-code.tar.xz"
+        sha256 "4c083722a0dcb54dcb8827198311075111046d170ab48d595a4b36a1501dce4a"
+    end
+
+    on_linux do
+        url "https://fornwall.me/tmp/advent-of-code-linux.tar.xz"
+        sha256 "bd249f06d1385aa0ad6d8176d6ebf22ccd1b0719197ad3661e150e3f57d59aee"
+    end
 
     def install
         bin.install "advent-of-code"
