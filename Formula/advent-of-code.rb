9655e78787d88a77faef7016fd3c7405dc52c4f8
Fredrik Fornwall <fredrik@fornwall.net>
2022-12-05 23:59:58 +0100

    Add script to regenerate formula

--- Formula/advent-of-code.rb
+++ Formula/advent-of-code.rb
@@ -2,16 +2,28 @@ class AdventOfCode < Formula
     desc "CLI to solve Advent of Code problems"
     homepage "https://aoc.fornwall.net"
 
-    version "0.1"
+    version "0.1.1670281190"
 
     on_macos do
-        url "https://fornwall.me/tmp/advent-of-code.tar.xz"
-        sha256 "4c083722a0dcb54dcb8827198311075111046d170ab48d595a4b36a1501dce4a"
+        on_arm do
+            url "https://aoc.fornwall.net/release/advent-of-code-mac-aarch64.tar.xz"
+            sha256 "5a234b0d743d89f5f7c7ec969e96126164948d68c4bae422b75c24f47311ef2b"
+        end
+        on_intel do
+            url "https://aoc.fornwall.net/release/advent-of-code-mac-x86_64.tar.xz"
+            sha256 "de951558d8ec3daaee60469cae0aef7b9b9a8776c0a1d5d05a9a4a2876456570"
+        end
     end
 
     on_linux do
-        url "https://fornwall.me/tmp/advent-of-code-linux.tar.xz"
-        sha256 "bd249f06d1385aa0ad6d8176d6ebf22ccd1b0719197ad3661e150e3f57d59aee"
+        on_arm do
+            url "https://aoc.fornwall.net/release/advent-of-code-linux-aarch64.tar.xz"
+            sha256 "480c672cc501e26ae75f556e82dd14176b93bdfcef7c091be682b907c697ac0b"
+        end
+        on_intel do
+            url "https://aoc.fornwall.net/release/advent-of-code-linux-x86_64.tar.xz"
+            sha256 "a363cf5d9732dadc0d6ae29e8d20aab5257c48cebbda246e131b05066d0f6741"
+        end
     end
 
     def install
---
+++ scripts/regenerate-advent-of-code.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import time
+import urllib.request
+from hashlib import sha256
+
+hashes = {}
+urls = {}
+
+for os in ['linux', 'mac']:
+    hashes[os] = {}
+    urls[os] = {}
+    for arch in ['aarch64', 'x86_64']:
+        url = f'https://aoc.fornwall.net/release/advent-of-code-{os}-{arch}.tar.xz'
+        request = urllib.request.Request(url, data=None, headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+        })
+        file_bytes = urllib.request.urlopen(request).read()
+        h1 = sha256()
+        h1.update(file_bytes)
+        hashes[os][arch] = h1.hexdigest()
+        urls[os][arch] = url
+
+version = '0.1.' + str(int(time.time()))
+
+formula = f"""class AdventOfCode < Formula
+    desc "CLI to solve Advent of Code problems"
+    homepage "https://aoc.fornwall.net"
+
+    version "{version}"
+
+    on_macos do
+        on_arm do
+            url "{urls['mac']['aarch64']}"
+            sha256 "{hashes['mac']['aarch64']}"
+        end
+        on_intel do
+            url "{urls['mac']['x86_64']}"
+            sha256 "{hashes['mac']['x86_64']}"
+        end
+    end
+
+    on_linux do
+        on_arm do
+            url "{urls['linux']['aarch64']}"
+            sha256 "{hashes['linux']['aarch64']}"
+        end
+        on_intel do
+            url "{urls['linux']['x86_64']}"
+            sha256 "{hashes['linux']['x86_64']}"
+        end
+    end
+
+    def install
+        bin.install "advent-of-code"
+    end
+end"""
+
+print(formula)
