d1f9051c8dd7bf25cca47d9ed3f96273d65941e5
Fredrik Fornwall <fredrik@fornwall.net>
2024-11-14 21:35:09 +0100

    Update formulas from scheduled job

--- Formula/luau-lsp.rb
+++ Formula/luau-lsp.rb
@@ -2,17 +2,17 @@ class LuauLsp < Formula
     desc "Language Server Implementation for Luau"
     homepage "https://github.com/JohnnyMorganz/luau-lsp"
 
-    version "1.28.1"
+    version "1.35.0"
 
     on_macos do
-        url "https://github.com/JohnnyMorganz/luau-lsp/releases/download/1.28.1/luau-lsp-macos.zip"
-        sha256 "2e6ee111eea95643abe1e22927a0c1daadb5f342c5668dfd4eb090e9407c93e5"
+        url "https://github.com/JohnnyMorganz/luau-lsp/releases/download/1.35.0/luau-lsp-macos.zip"
+        sha256 "2b4a1f43fc95bbf5027b8635c134d7e8f3d59f7ff4c35562d1765e9ec11c54e8"
     end
 
     on_linux do
         on_intel do
-            url "https://github.com/JohnnyMorganz/luau-lsp/releases/download/1.28.1/luau-lsp-linux.zip"
-            sha256 "f332716a1ead1d5a9e352d6c836d8e50b5556710c110b0590acba656689c5041"
+            url "https://github.com/JohnnyMorganz/luau-lsp/releases/download/1.35.0/luau-lsp-linux.zip"
+            sha256 "53b0b4287d161743151a8eeb62465575dc58a5142eaaca9f58de591baaf7ca7f"
         end
     end
 
